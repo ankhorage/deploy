7589ba252266bb7632a974dc6b8c851546f5c1e9
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-08-12 11:06:33 +0200

    feat: publish deployment domain api

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,15 @@ import { expect, test } from 'bun:test';
 
 import * as deploy from './index.js';
 
-test('package entrypoint is loadable', () => {
-  expect(deploy).toEqual({});
+test('package entrypoint exposes the canonical deployment domain', () => {
+  expect(Object.keys(deploy).sort()).toEqual([
+    'DEPLOYMENT_CAPABILITIES',
+    'DEPLOYMENT_CHANGE_KINDS',
+    'DEPLOYMENT_CHANGE_REASONS',
+    'DEPLOYMENT_PLAN_DIAGNOSTIC_CODES',
+    'DEPLOYMENT_STEP_OPERATIONS',
+    'createDeploymentChanges',
+    'createDeploymentPlan',
+    'executeDeploymentPlan',
+  ]);
 });
