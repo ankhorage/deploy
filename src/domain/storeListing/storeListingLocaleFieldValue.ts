c08137dd66071a9bfd99225b1e74b6045515f084
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-08-13 17:30:56 +0200

    fix: avoid dynamic listing field access

--- src/domain/storeListing/storeListingLocaleFieldValue.ts
+++ src/domain/storeListing/storeListingLocaleFieldValue.ts
@@ -5,5 +5,24 @@ export function storeListingLocaleFieldValue(
   listing: StoreListingLocale,
   field: StoreListingField,
 ): string | readonly string[] | undefined {
-  return listing[field];
+  switch (field) {
+    case 'name':
+      return listing.name;
+    case 'summary':
+      return listing.summary;
+    case 'description':
+      return listing.description;
+    case 'keywords':
+      return listing.keywords;
+    case 'promotionalText':
+      return listing.promotionalText;
+    case 'supportUrl':
+      return listing.supportUrl;
+    case 'marketingUrl':
+      return listing.marketingUrl;
+    case 'privacyPolicyUrl':
+      return listing.privacyPolicyUrl;
+    case 'promoVideoUrl':
+      return listing.promoVideoUrl;
+  }
 }
