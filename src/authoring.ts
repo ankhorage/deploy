015c0752476ac494439d535f512eea0bc8f0e069
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-25 04:12:40 +0200

    feat(authoring): expose deploy authoring owner API

---
+++ src/authoring.ts
@@ -0,0 +1,11 @@
+export { DEPLOY_AUTHORING_STRUCTURE } from './features/authoring/domain/deployAuthoringStructure';
+export { fromDeployMonetizationAuthoringValue } from './features/authoring/application/use-cases/fromDeployMonetizationAuthoringValue';
+export { fromDeployReleaseAuthoringValue } from './features/authoring/application/use-cases/fromDeployReleaseAuthoringValue';
+export { toDeployMonetizationAuthoringValue } from './features/authoring/application/use-cases/toDeployMonetizationAuthoringValue';
+export { toDeployReleaseAuthoringValue } from './features/authoring/application/use-cases/toDeployReleaseAuthoringValue';
+export type {
+  DeployMonetizationAuthoringValue,
+  DeployMonetizationProductAuthoringValue,
+  DeployReleaseAuthoringRollout,
+  DeployReleaseAuthoringValue,
+} from './types/deployAuthoring';
