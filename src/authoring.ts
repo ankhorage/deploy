2fb46a377489d467494d295307683da00ab598ce
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-25 04:16:16 +0200

    fix(authoring): satisfy canonical lint and formatting

--- src/authoring.ts
+++ src/authoring.ts
@@ -1,8 +1,8 @@
-export { DEPLOY_AUTHORING_STRUCTURE } from './features/authoring/domain/deployAuthoringStructure';
 export { fromDeployMonetizationAuthoringValue } from './features/authoring/application/use-cases/fromDeployMonetizationAuthoringValue';
 export { fromDeployReleaseAuthoringValue } from './features/authoring/application/use-cases/fromDeployReleaseAuthoringValue';
 export { toDeployMonetizationAuthoringValue } from './features/authoring/application/use-cases/toDeployMonetizationAuthoringValue';
 export { toDeployReleaseAuthoringValue } from './features/authoring/application/use-cases/toDeployReleaseAuthoringValue';
+export { DEPLOY_AUTHORING_STRUCTURE } from './features/authoring/domain/deployAuthoringStructure';
 export type {
   DeployMonetizationAuthoringValue,
   DeployMonetizationProductAuthoringValue,
